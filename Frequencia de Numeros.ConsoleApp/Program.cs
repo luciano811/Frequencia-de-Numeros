1579bb5db80cf59bac5496c08ecb4676c48bd622
luciano811 <xlucianox811@gmail.com>
2022-02-22 16:45:10 -0300

    Consertei os bugs

--- Frequencia de Numeros.ConsoleApp/Program.cs
+++ Frequencia de Numeros.ConsoleApp/Program.cs
@@ -14,100 +14,47 @@ namespace Frequencia_de_Numeros.ConsoleApp
 
             Console.WriteLine("Digite os numeros separados por espaço");
 
-            string [] strNumeros = Console.ReadLine().Split(' ');
-            int [] numeros = new int[qtd];
-                        
+            string[] strNumeros = Console.ReadLine().Split(' ');
+            int[] numeros = new int[qtd];
 
-            for (int i = 0; i < strNumeros.Length; i++) 
+
+            for (int i = 0; i < strNumeros.Length; i++)
             {
                 numeros[i] = Convert.ToInt32(strNumeros[i]);
                 Console.WriteLine(numeros[i]);
             }
 
-            Array.Sort(numeros);
+
 
             Console.WriteLine("\n\nOrganizando por ordem crescente\n");
+            Array.Sort(numeros);
+
+            for (int i = 0; i < numeros.Length; i++)
+                Console.WriteLine(numeros[i]);
+
+            Console.WriteLine();
 
 
-                                                       
-            
-            for (int i = 0;i <= qtd; i = i++)
+            for (int i = 0; i < qtd; i++)
             {
 
-                if (numeros[i] == numeros[i++])
-                {
-                    x++;
-                    
-                }
-                
+                if (i != qtd - 1)
+                    if (numeros[i] == numeros[i + 1])
+                    {
 
+                        x++;
+                        continue;
 
-                Console.WriteLine($"O número {numeros[i]} aparece {x} vezes ");
+                    }
+
+                    else if (numeros[i] == numeros[qtd - 1])
+                    {
+                        Console.WriteLine($"O número {numeros[i]} aparece {x} vezes ");
+                    }
+
+                Console.WriteLine($"O número {numeros[i]} aparece com a frequência: {x}");
                 x = 1;
             }
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            //int z = 5;
-            
-            //int[] numeros = new int[z];
-            //Random rm = new Random();
-
-            //for (int i = 0; i < numeros.Length; i++)
-            //{
-            //    numeros[i] = rm.Next(1, 25);
-            //    Console.WriteLine(numeros[i]);
-            //}
-
-            //for (int i = 0; i < numeros.Length; i++)
-            //{
-            //    if (numeros[i] < numeros[i++])
-            //    {
-            //        Console.WriteLine(numeros[i]);
-            //    }
-            //}
-
-
-
-
-
-
-
-
-
-
-
-            ////maior e menor altura
-            //double[] alturas = new double[15];
-
-            //double maiorAltura = alturas[0];
-            //double menorAltura = 1;
-
-            //int i = 0;
-            //do
-            //{
-            //    Console.Write("Qual a altura? ");
-            //    string strAltura = Console.ReadLine();
-            //    alturas[i] = Convert.ToDouble(strAltura);
-
-            //    if (alturas[i] <= menorAltura)
-            //        menorAltura = alturas[i];
-
-            //    if (alturas[i] > maiorAltura)
-            //        maiorAltura = alturas[i];
-
-            //    i++;
-            //} while (i < 15);
-
-            //Console.WriteLine("A maior altura é: " + maiorAltura);
-            //Console.WriteLine("A menor altura é: " + menorAltura);
-
         }
     }
 }
